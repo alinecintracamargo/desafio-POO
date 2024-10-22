fde248e1df216e00ca0ea27c34f3dd535cdd123b
alinecintracamargo <alineandradecintra@gmail.com>
2024-10-22 16:57:47 -0300

    feat: finished project

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -24,8 +26,44 @@ public class Main {
         mentoria.setData(LocalDate.now());
 
 
-        System.out.println(curso1);
+       /* System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devAline = new Dev();
+        devAline.setNome("Aline");
+        devAline.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Aline" + devAline.getConteudosInscritos());
+
+        devAline.progredir();
+        devAline.progredir();
+        System.out.println("--");
+        System.out.println("Conteúdos Inscritos Aline" + devAline.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Aline" + devAline.getConteudosConcluidos());
+        System.out.println("XP:" + devAline.calcularTotalXP());
+
+        System.out.println("-----------");
+
+        Dev devGeovana = new Dev();
+        devGeovana.setNome("Geovana");
+        devGeovana.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Geovana" + devGeovana.getConteudosInscritos());
+
+        devGeovana.progredir();
+        devGeovana.progredir();
+        devGeovana.progredir();
+        System.out.println("--");
+        System.out.println("Conteúdos Inscritos Geovana" + devGeovana.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Geovana" + devGeovana.getConteudosConcluidos());
+        System.out.println("XP:" + devGeovana.calcularTotalXP());
+
+
     }
 }
--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,8 +1,13 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
+public class Curso extends Conteudo {
+
+    private int cargaHoraria;
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
 
     public Curso() {
     }
@@ -10,26 +15,10 @@ public class Curso {
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
     }
 
     public int getCargaHoraria() {
@@ -40,5 +29,5 @@ public class Curso {
         this.cargaHoraria = cargaHoraria;
     }
 
-    private int cargaHoraria;
+
 }
